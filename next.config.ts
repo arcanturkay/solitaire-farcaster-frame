e752da871a302590cce656e75a92c48761cbc23d
Arcan Turkay <arcan.turkay@sahibinden.com>
2025-10-05 21:56:19 +0300

    fix-2

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,13 @@ const nextConfig: NextConfig = {
             },
         ];
     },
+    webpack(config) {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            'react-native-async-storage/async-storage': false, // uyarıyı engeller
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
